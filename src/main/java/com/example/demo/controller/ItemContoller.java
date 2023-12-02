d8633df6977d4aa41dd82ca44fe3c4de3fb3ad82
villageworks <villageworks@gmail.com>
2023-12-03 00:45:16 +0900

    ver.0.12: カテゴリ検索用リンク表示

--- src/main/java/com/example/demo/controller/ItemContoller.java
+++ src/main/java/com/example/demo/controller/ItemContoller.java
@@ -7,21 +7,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Category;
 import com.example.demo.entity.Item;
+import com.example.demo.repository.CategoryRepository;
 import com.example.demo.repository.ItemRepository;
 
 @Controller
 public class ItemContoller {
-	
+
+	@Autowired
+	CategoryRepository categoryRepository;
 	@Autowired
 	ItemRepository itemRepository;
 	
 	// 商品一覧表示
 	@GetMapping("/items")
 	public String index(Model model) {
+		// すべてのカテゴリを種痘
+		List<Category> categoryList = categoryRepository.findAll();
 		// すべての商品を取得
 		List<Item> itemList = itemRepository.findAll();
-		// スコープに取得した商品を登録
+		// 取得したカテゴリリストと取得した商品をスコープに登録
+		model.addAttribute("categoryList", categoryList);
 		model.addAttribute("itemList", itemList);
 		// 画面遷移
 		return "items";
