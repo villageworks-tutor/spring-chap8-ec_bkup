a075d8ebe36f1a03316857789b3a8088ccf1a852
villageworks <villageworks@gmail.com>
2023-12-06 01:20:21 +0900

    ver.0.33: 顧客情報永続化

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Customer;
+import com.example.demo.repository.CustomerRepository;
 
 @Controller
 public class OrderController {
+	
+	@Autowired
+	CustomerRepository customerRepository;
+	
 	// 顧客情報入力画面表示
 	@GetMapping("/order")
 	public String index() {
@@ -31,5 +37,23 @@ public class OrderController {
 		model.addAttribute("customer", customer);
 		// 画面遷移
 		return "orderConfirm";
+	}
+	
+	// 注文確定処理
+	@PostMapping("/order")
+	public String doOrder(
+			@RequestParam("name") String name,
+			@RequestParam("address") String address,
+			@RequestParam("tel") String tel,
+			@RequestParam("email") String email,
+			Model model) {
+		// リクエストパラメータをもとに顧客クラスをインスタンス化
+		Customer customer = new Customer(name, address, tel, email);
+		// 顧客インスタンスを永続化
+		customerRepository.save(customer);
+
+		
+		// 画面遷移
+		return "ordered";
 	}
 }
